b6b4cfa6f1e8ee3d82859700fd57589eebaf5412
Pabitra Kumar Mondal <36961893+PabitraKumarMondal@users.noreply.github.com>
2020-04-23 10:58:43 +0530

    Handson 1

---
+++ Handson 1/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,36 @@
+package com.cognizant.ormlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+	
+	private static CountryService countryService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+	public static void main(String[] args) {
+		//SpringApplication.run(OrmLearnApplication.class, args);
+		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+		countryService = context.getBean(CountryService.class);
+		LOGGER.info("Inside main");
+		testGetAllCountries();
+	}
+	
+	private static void testGetAllCountries() {
+		LOGGER.info("Start");
+		List<Country> countries = countryService.getAllCountries();
+		LOGGER.debug("countries={}", countries);
+		LOGGER.info("End");
+	}
+
+}
---
+++ Handson 1/orm-learn/src/test/java/com/cognizant/ormlearn/OrmLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cognizant.ormlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class OrmLearnApplicationTests {
+
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
